6f32e38062c6cc62254f8513e1d4a27d4d5c4d54
Jackmc <jackmc1122@gmail.com>
2019-07-25 13:34:53 -0700

    Add GET studio by id route.

--- test/studios.test.js
+++ test/studios.test.js
@@ -1,7 +1,10 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const Studio = require('../lib/models/Studio');
+
 describe('studio routes', () => {
+
   it('creates and returns a studio', () => {
     return request(app)
       .post('/api/v1/studios')
@@ -30,6 +33,21 @@ describe('studio routes', () => {
         expect(res.body[0]).toEqual({
           _id: expect.any(String),
           name: expect.any(String)
+        });
+      });
+  });
+
+  it('returns a studio by its id', async() => {
+    const { _id } = await Studio.findOne();
+    return request(app)
+      .get(`/api/v1/studios/${_id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: expect.any(String),
+          city: expect.any(String),
+          state: expect.any(String),
+          country: expect.any(String),
         });
       });
   });
